0b4ab4e96c3421012aadef28a561d7f975847e66
J. Kyle Fagan <jkyle@causethought.com>
2016-03-09 18:26:22 -0600

    Added data structures to scratch pad directory.

---
+++ scratch-pad/data-structures.js
@@ -0,0 +1,16 @@
+var card = {
+	Name: '',
+	Type: '',
+	Effect: '',
+	CMC: '',
+	WhiteIntencity: '',
+	GreenIntencity: '',
+	RedIntencity: '',
+	BlackIntencity: '',
+	BlueIntencity: '',
+	ColorlessIncencity: '',
+	AnyIntencity: '',
+	Price: ''
+};
+
+var deck = [];
