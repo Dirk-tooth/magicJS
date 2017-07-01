1374ffaca17ba8596f20a9237f8ff7d764541e4e
Dirk-Tooth <trevormckay.davis@gmail.com>
2017-07-01 13:53:15 -0500

    Removed unnecessary logs

--- utility/requests.js
+++ utility/requests.js
@@ -1,18 +1,17 @@
 const $ = require('jquery');
-const planes = require('../data/planes.json');
-const jace = require('../data/jace.json');
+// const planes = require('../data/planes.json');
+// const jace = require('../data/jace.json');
 
 function requestLayout(layout) {
-  console.log($.getJSON(`https://api.magicthegathering.io/v1/cards?layout=${layout}&set=pca`));
-	// TODO: include "set: pca" in request
+	// console.log($.getJSON(`https://api.magicthegathering.io/v1/cards?layout=${layout}&set=pca`));
   return $.getJSON(`https://api.magicthegathering.io/v1/cards?layout=${layout}&set=pca`);
 	// console.log(planes);
 	// return new Promise(resolve => resolve(planes));
 }
 
 function searchRequest(searchType, searchText) {
-	// return $.getJSON(`https://api.magicthegathering.io/v1/cards?${searchType}=${searchText}`);
-  return new Promise(resolve => resolve(jace));
+  return $.getJSON(`https://api.magicthegathering.io/v1/cards?${searchType}=${searchText}`);
+	// return new Promise(resolve => resolve(jace));
 }
 
 module.exports.layout = requestLayout;
